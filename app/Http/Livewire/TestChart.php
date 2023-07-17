4ecf3f09880b29d1f2ad3d9e23b92666cb8c7b05
AshDest <ashuzamaheshe4@gmail.com>
2023-07-17 16:58:30 +0200

    initial commit

---
+++ app/Http/Livewire/TestChart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TestChart extends Component
+{
+    public function render()
+    {
+        return view('livewire.test-chart');
+    }
+}
